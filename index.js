b8ce5b0c724153c91fc9d7cfe3a8739bb5191fbb
Nomfundo Florence <nomfundomlangeni0@gmail.com>
2024-08-11 03:33:53 +0200

    burger menu & projects section

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 document.addEventListener('DOMContentLoaded', (event) => {
+  const burgerMenu = document.getElementById('burger-menu');
+  const navLinks = document.getElementById('nav-links');
+
+  burgerMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    burgerMenu.classList.toggle('active');
+  });
+
   document.getElementById('phoneNumber').addEventListener('click', () => {
     const phoneNumber = '+27-78-746-0853';
 
