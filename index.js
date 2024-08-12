c6ecf8ca602b263dc3870bfadd92861f17da2301
Nomfundo Florence <nomfundomlangeni0@gmail.com>
2024-08-12 10:06:42 +0200

    enlarged project's area & modified burger menu to close

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 document.addEventListener('DOMContentLoaded', (event) => {
   const burgerMenu = document.getElementById('burger-menu');
   const navLinks = document.getElementById('nav-links');
+  const navItems = navLinks.getElementsByTagName('a');
 
   burgerMenu.addEventListener('click', () => {
     navLinks.classList.toggle('active');
     burgerMenu.classList.toggle('active');
   });
+
+  for (let item of navItems) {
+    item.addEventListener('click', () => {
+      navLinks.classList.remove('active');
+      burgerMenu.classList.remove('active');
+    });
+  }
 
   document.getElementById('phoneNumber').addEventListener('click', () => {
     const phoneNumber = '+27-78-746-0853';
